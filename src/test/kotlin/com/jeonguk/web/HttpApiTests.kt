b5ce9a545899c61b46d8be0f032cd7b6110d576a
jeonguk <kato1883@gmail.com>
2019-03-10 16:35:19 +0900

    Test code edit

--- src/test/kotlin/com/jeonguk/web/HttpApiTests.kt
+++ src/test/kotlin/com/jeonguk/web/HttpApiTests.kt
@@ -2,7 +2,6 @@ package com.jeonguk.web
 
 import com.jeonguk.web.config.WebConfig
 import com.jeonguk.web.model.User
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
@@ -12,25 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 @ExtendWith(SpringExtension::class)
-@WebAppConfiguration
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = [WebConfig::class])
 @SpringBootTest(classes = [Application::class])
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class HttpApiTests(@Autowired val wac: WebApplicationContext, @Autowired var mockMvc: MockMvc) {
-
-    @BeforeEach
-    fun setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build()
-    }
+class HttpApiTests(@Autowired var mockMvc: MockMvc) {
 
     @Test
     fun `List users`() {
@@ -41,6 +32,7 @@ class HttpApiTests(@Autowired val wac: WebApplicationContext, @Autowired var moc
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("\$.[0].user_id").value(user.userId))
                 .andExpect(jsonPath("\$.[0].user_name").value(user.userName))
+                .andDo(MockMvcResultHandlers.print())
     }
 
 
--- src/test/kotlin/com/jeonguk/web/IntegrationTests.kt
+++ src/test/kotlin/com/jeonguk/web/IntegrationTests.kt
@@ -3,12 +3,14 @@ package com.jeonguk.web
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IntegrationTests {
 
     @BeforeAll
