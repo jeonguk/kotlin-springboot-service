fa59dcab84ce061ff80ec964ea588f0f234bd960
jeonguk <kato1883@gmail.com>
2019-03-15 18:11:45 +0900

    H2 Db config add

--- src/test/kotlin/com/jeonguk/web/ApplicationTests.kt
+++ src/test/kotlin/com/jeonguk/web/ApplicationTests.kt
@@ -1,10 +1,11 @@
 package com.jeonguk.web
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
 
-
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApplicationTests {
 
 	@Test
