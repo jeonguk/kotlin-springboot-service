230d4fee62fee28f425ae51f049bb88eaed8a4e4
jeonguk <kato1883@gmail.com>
2019-03-15 18:14:01 +0900

    Configuration edit

--- src/main/kotlin/com/jeonguk/web/Application.kt
+++ src/main/kotlin/com/jeonguk/web/Application.kt
@@ -2,11 +2,22 @@ package com.jeonguk.web
 
 import com.jeonguk.web.model.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import java.util.concurrent.ConcurrentHashMap
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = [
+			DataSourceAutoConfiguration::class,
+			HibernateJpaAutoConfiguration::class,
+			DataSourceTransactionManagerAutoConfiguration::class],
+		scanBasePackages = ["com.jeonguk.web"]
+)
+@EnableTransactionManagement
 class Application {
 
 	companion object {
