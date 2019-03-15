cbd4044bdf5d8d2025e33a520d68236bc8b9d526
barry <barry.allen@kakaobank.com>
2019-03-15 18:10:36 +0900

    H2 Db config add

---
+++ src/main/kotlin/com/jeonguk/web/config/db/H2DbConfig.kt
@@ -0,0 +1,53 @@
+package com.jeonguk.web.config.db
+
+import com.google.common.collect.ImmutableMap
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import javax.sql.DataSource
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = ["com.jeonguk.web.repository"],
+        entityManagerFactoryRef = "entityManager",
+        transactionManagerRef = "transactionManager"
+)
+@EnableTransactionManagement
+class H2DbConfig {
+    @Bean
+    fun dataSource(): DataSource {
+        val dataSource = DriverManagerDataSource()
+        dataSource.setDriverClassName("org.h2.Driver")
+        dataSource.url = "jdbc:h2:mem:mojo-app;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
+        dataSource.username = "sa"
+        dataSource.password = ""
+        return dataSource
+    }
+
+    @Bean
+    fun entityManager(): LocalContainerEntityManagerFactoryBean {
+        val em = LocalContainerEntityManagerFactoryBean()
+        em.dataSource = dataSource()
+        em.setPackagesToScan("com.kakaobank.mojo.entity.shard")
+        em.jpaVendorAdapter = HibernateJpaVendorAdapter()
+        em.setJpaPropertyMap(ImmutableMap.of(
+                "hibernate.hbm2ddl.auto", "create-drop",
+                "hibernate.dialect", "org.hibernate.dialect.H2Dialect",
+                "hibernate.ejb.entitymanager_factory_name", "entityManager"
+        ))
+        return em
+    }
+
+    @Bean
+    fun transactionManager(): PlatformTransactionManager {
+        val transactionManager = JpaTransactionManager()
+        transactionManager.entityManagerFactory = entityManager().getObject()
+        return transactionManager
+    }
+}
