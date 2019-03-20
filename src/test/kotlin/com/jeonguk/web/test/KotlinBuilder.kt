b9bb7eb463460204a513c3f5c288194242f827aa
jeonguk <kato1883@gmail.com>
2019-03-20 15:32:16 +0900

    kotlin 빌더 패턴 테스트

---
+++ src/test/kotlin/com/jeonguk/web/test/KotlinBuilder.kt
@@ -0,0 +1,36 @@
+package com.jeonguk.web.test
+
+/**
+ * kotlin builder pattern class tset
+ */
+class KotlinBuilder(
+        private val userId: String = "",
+        private val userName: String = "",
+        private val age: Int?) {
+
+        data class Builder(
+            var userId: String = "",
+            var userName: String = "",
+            var age: Int = 0) {
+
+                fun userId(userId: String) = apply { this.userId = userId }
+                fun userName(userName: String) = apply { this.userName = userName }
+                fun age(age: Int) = apply { this.age = age }
+                fun build() = KotlinBuilder(userId, userName, age)
+        }
+
+    override fun toString(): String {
+        return "userId = $userId userName = $userName age = $age"
+    }
+}
+
+// Usages
+fun main() {
+
+    val builterTest = KotlinBuilder.Builder()
+            .userId("write user id")
+            .userName("jeonguk")
+            .build()
+    println(builterTest)
+
+}
